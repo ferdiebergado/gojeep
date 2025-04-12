44c3815cd6e03a8be93307c77fb16f2d450c91b2
ferdie bergado <ferdiebergado@gmail.com>
2025-04-12 18:34:14 +0800

    test: Add unit  test for Argon2Hasher.Verify()

--- internal/pkg/security/hashing_test.go
+++ internal/pkg/security/hashing_test.go
@@ -14,8 +14,22 @@ func TestArgon2HasherHash(t *testing.T) {
 	password := "securepassword"
 
 	hashed, err := hasher.Hash(password)
-
 	assert.NoError(t, err, "Hashing should not return an error")
 	assert.NotEmpty(t, hashed, "Hashed password should not be empty")
 	assert.True(t, strings.HasPrefix(hashed, "$argon2id$"), "Hashed password should have the correct prefix")
+}
+
+func TestArgon2Hasher_Verify(t *testing.T) {
+	t.Parallel()
+	hasher := &security.Argon2Hasher{}
+	password := "securepassword"
+
+	hashed, err := hasher.Hash(password)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ok, err := hasher.Verify(password, hashed)
+	assert.NoError(t, err, "Hash verify should not return an error")
+	assert.True(t, ok, "Hashed password should match the plain text password")
 }
