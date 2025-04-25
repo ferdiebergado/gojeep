03ad840f04cc789593231a25ffd7bd511ddc4cfc
ferdie bergado <ferdiebergado@gmail.com>
2025-04-25 08:53:05 +0000

    refactor: Rephrase regSuccess message for clarity

--- internal/pkg/message/message.go
+++ internal/pkg/message/message.go
@@ -7,7 +7,7 @@ const (
 	UserInputInvalid  = "Invalid input."
 	UserLoginSuccess  = "Login successful!"
 	UserNotFound      = "Invalid username or password."
-	UserRegSuccess    = "Thank you for registering. Please check your email for the verification link."
+	UserRegSuccess    = "A link to activate your account has been emailed to the address provided."
 	UserUnverified    = "Please verify your email."
 	UserVerifySuccess = "Verification successful!"
 )
